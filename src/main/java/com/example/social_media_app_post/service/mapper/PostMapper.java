ebc7b32e05daeaf4d60d2ccf0580cf6cdb037445
MQuan <minhquantspt1@gmail.com>
2024-11-08 20:54:04 +0700

    .

--- src/main/java/com/example/social_media_app_post/service/mapper/PostMapper.java
+++ src/main/java/com/example/social_media_app_post/service/mapper/PostMapper.java
@@ -1,6 +1,7 @@
 package com.example.social_media_app_post.service.mapper;
 
 import com.example.social_media_app_post.dto.post.CreatePostInput;
+import com.example.social_media_app_post.dto.post.PostOutput;
 import com.example.social_media_app_post.entity.PostEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
@@ -11,5 +12,5 @@ public interface PostMapper {
 //    PostEntity getEntityFromInput(CreatePostGroupInput createPostGroupInput);
 //    void updateEntityFromInput(@MappingTarget PostEntity postEntity, CreatePostInput createPostInput);
 //    void updateEntityFromInput(@MappingTarget PostEntity postEntity, CreatePostGroupInput createPostGroupInput);
-//    PostOutput getOutputFromEntity(PostEntity postEntity);
+  PostOutput getOutputFromEntity(PostEntity postEntity);
 }
